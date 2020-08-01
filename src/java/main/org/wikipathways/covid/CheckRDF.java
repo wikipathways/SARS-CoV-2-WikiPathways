f29347271facfba31c9739c0c891d500c498a8d7
Egon Willighagen <egon.willighagen@gmail.com>
2020-08-01 16:50:26 +0200

    More informative report

--- src/java/main/org/wikipathways/covid/CheckRDF.java
+++ src/java/main/org/wikipathways/covid/CheckRDF.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import nl.unimaas.bigcat.wikipathways.curator.assertions.*;
-import nl.unimaas.bigcat.wikipathways.curator.tests.GeneTests;
+import nl.unimaas.bigcat.wikipathways.curator.tests.*;
 import nl.unimaas.bigcat.wikipathways.curator.SPARQLHelper;
 
 import org.apache.jena.rdf.model.Model;
@@ -25,28 +25,62 @@ public class CheckRDF {
         loadedData.read(new FileInputStream(new File(gpmlFile)), "", "TURTLE");
 
         SPARQLHelper helper = new SPARQLHelper(loadedData);
-        assertions.addAll(GeneTests.entrezGeneIdentifiersNotNumber(helper));
-        
+        assertions.addAll(GeneTests.all(helper));
+        assertions.addAll(InteractionTests.all(helper));
+
+        System.out.println("## Tests");
+
         int failed = 0;
+        int testClasses = 0;
+        int tests = 0;
+        String currentTestClass = "";
+        String currentTest = "";
+        String message = "";
+        String errors = "";
         for (IAssertion assertion : assertions) {
+            if (assertion.getTestClass() != currentTestClass) {
+                currentTestClass = assertion.getTestClass();
+                currentTest = "";
+                testClasses++;
+                if (!errors.isEmpty()) message += "\n" + errors;
+                if (!message.isEmpty()) System.out.println(message);
+                message = "";
+                System.out.println("\n* " + currentTestClass);
+            }
+            if (assertion.getTest() != currentTest) {
+                currentTest = assertion.getTest();
+                if (!message.isEmpty()) System.out.println(message);
+                message = "** " + currentTest + ": ";
+                errors = "";
+                tests++;
+            }
             if (assertion instanceof AssertEquals) {
                 AssertEquals typedAssertion = (AssertEquals)assertion;
                 if (!typedAssertion.getExpectedValue().equals(typedAssertion.getValue())) {
-                    System.out.println(typedAssertion.getMessage());
-                    failed++;
+                   message += "x";
+                   errors += "*** " + typedAssertion.getMessage();
+                   failed++;
+                } else {
+                    message += ".";
                 }
             } else if (assertion instanceof AssertNotNull) {
                 AssertNotNull typedAssertion = (AssertNotNull)assertion;
                 if (typedAssertion.getValue() == null) {
-                    System.out.println("Unexpected null found");
-                    failed++;
+                   message += "x";
+                   errors += "*** Unexpected null found";
+                   failed++;
+                } else {
+                    message += ".";
                 }
             } else {
-                System.out.println("Unrecognized assertion type: " + assertion.getClass().getName());
+                errors += "*** Unrecognized assertion type: " + assertion.getClass().getName();
                 failed++;
             }
         }
-        System.out.println("## Summary\n");
+        if (!message.isEmpty()) System.out.println(message);
+        System.out.println("\n## Summary\n");
+        System.out.println("* Number of test classes: " + testClasses);
+        System.out.println("* Number of tests: " + tests);
         System.out.println("* Number of assertions: " + assertions.size());
         System.out.println("* Number of fails: " + failed);
     }
